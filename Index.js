e48c4688807fd9fdb0c64342b4b59a878762b5c6
RominikMK <rominikroberto@prof.educacao.sp.gov.br>
2024-05-23 11:19:20 -0300

    CSS

---
+++ Index.js
@@ -0,0 +1,42 @@
+let firstName = "ROMINIK";
+let lastname = "MACIEL";
+console.log(firstName);
+console.log(lastname);
+
+//let price = 30;
+//price = 20;
+//console.log(price); mostrando numero
+
+const price = 30;
+//price = 20;
+console.log(price)
+
+// ponto e virgula não precisa colocar que não vai influenciar em nada.
+
+// Tipos de vriaveis
+
+//ESTATICO E DINAMICO
+//console .log(typeof itemprice)
+
+//
+//let itemname = "pen"
+//let itemPrice = 3
+//let itemAvailable = true
+//let itemColor = "red"
+//itemPrice = "dois"
+
+//console.log(typeof itemPrice)//
+
+
+let pen = { 
+    itemname: "pen",
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: "red",
+    
+    
+
+}
+
+pen.
+console.log(pen)
